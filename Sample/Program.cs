20d0479ee995aa0500cc4930d5cec8d6eb7ea5c6
saltukkos <saltukkos@gmail.com>
2019-11-07 23:59:12 +0700

    init

---
+++ Sample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Sample
+{
+    public interface IFoo<T>
+    {
+        sealed Type GetItemType() => typeof(T);
+    }
+
+    public sealed class Foo : IFoo<string>
+    {
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IFoo<string> foo = new Foo();
+            foo.GetItemType();
+        }
+    }
+}
